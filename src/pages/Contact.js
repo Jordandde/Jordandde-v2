dec74eeb27b400a0583141c979b91751f65bc267
Jordandde <43556268+Jordandde@users.noreply.github.com>
2023-02-20 07:15:26 -0500

    Update Contact.js
    Updated resume link

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,7 +10,7 @@ import LeftCard from "../components/leftCard";
 import RightCard from "../components/rightCard";
 import { inject } from '@vercel/analytics';
 
-const hrefs = ["mailto:jordan.de.souza@uwaterloo.ca?subject=Hello","https://drive.google.com/file/d/1i9E5LCxsLck9wkxF3yx9qM2r9KDheXbr/view?usp=sharing"];
+const hrefs = ["mailto:jordan.de.souza@uwaterloo.ca?subject=Hello","https://drive.google.com/file/d/1S2JHkTN-QNAuVfVhQeJBfTS9fG5saA7x/view?usp=share_link"];
 const descriptions = ["Email: jordan.de.souza@uwaterloo.ca\n\n Phone: \n 647 839 9032", "Click on this box to be redirected to my Resumé, hosted on Google Drive"];
 const titles = ["Contact Info","Resume"]
 const Page4 = () => {
